984b3248e289df95ea4dec4871fc745f8abdecb5
Robin Stoebe <cbstoebe@gmail.com>
2025-04-21 16:02:40 -0700

    Connected Database

--- htdocs/config/db.php
+++ htdocs/config/db.php
@@ -1,19 +1,19 @@
 <?php
 require_once 'vault.php';
-$user=getpw('sysops/data/webapps/test','dbuser');
-$pass=getpw('sysops/data/webapps/test','dbpass');
+
+$user=getpw('sysops/data/webapps/test','dbuser') ?? 'antarchive';
+$pass=getpw('sysops/data/webapps/test','dbpass') ?? 'fMExIRG.)VRYUv_u';
 return [
     'class' => 'yii\db\Connection',
-
-    'dsn' => 'mysql:host=mysql.ics.uci.edu;dbname=test',
+    'dsn' => 'mysql:host=mysql.ics-db00.uci.edu;dbname=antarchive',
 
     'username' => $user,
     'password' => $pass,
     'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
 ];
 
